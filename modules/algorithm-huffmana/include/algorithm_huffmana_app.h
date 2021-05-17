cc87a1a7c80e0611d6e3c2244bdb2d0c94a0c454
shekotilova-yuliya <shekotilova.yuliya@mail.ru>
2021-05-17 17:41:44 +0300

    fix2

--- modules/algorithm-huffmana/include/algorithm_huffmana_app.h
+++ modules/algorithm-huffmana/include/algorithm_huffmana_app.h
@@ -7,15 +7,19 @@
 #include <vector>
 
 class AlgorithmHuffmanaApp {
+
 private:
+
   std::string message_;
   std::string convert(std::vector<std::string> _part_of_result);
   bool validateNumberOfArguments(int argc, const char** argv);
   void help(const char* appname, const char* message = "");
 
 public:
+
   AlgorithmHuffmanaApp();
   std::string operator()(int argc, const char** argv);
+
 };
 
 #endif  // MODULES_ALGORITHM_HUFFMANA_INCLUDE_ALGORITHM_HUFFMANA_APP_H_
